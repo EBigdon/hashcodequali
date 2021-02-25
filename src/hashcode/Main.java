92d6d71164039b359cd4b0ba37828dfe97aaf9c6
Erik <erik@bigdon.de>
2021-02-25 19:21:17 +0100

    Read from File
    Can read from file

--- src/hashcode/Main.java
+++ src/hashcode/Main.java
@@ -7,6 +7,7 @@ public class Main {
      * @param args standard arguments
      */
     public static void main(final String[] args) {
+        /*
         System.out.println("Hello");
         FileManager.createFile("testFile.txt");
         FileManager.clearFile("testFile.txt");
@@ -15,5 +16,45 @@ public class Main {
         FileManager.writeIntoFile("testFile.txt", "Test line 2");
         FileManager.writeIntoFile("testFile.txt", "Test line 3");
         FileManager.readFile("testFile.txt");
+        */
+        getInputFromFile("a.txt");
+    }
+
+    public static void getInputFromFile(final String fileName){
+        String output = FileManager.readFile(fileName);
+        String lines[] = output.split("\n");
+        int duration = Integer.parseInt(lines[0].split(" ")[0]);
+        int numberIntersection = Integer.parseInt(lines[0].split(" ")[1]);
+        int numberOfCars = Integer.parseInt(lines[0].split(" ")[2]);
+        int scoreForReachingEnd = Integer.parseInt(lines[0].split(" ")[3]);
+        System.out.println(duration);
+        System.out.println(numberIntersection);
+        System.out.println(numberOfCars);
+        System.out.println(scoreForReachingEnd);
+        getIntersections(lines,numberIntersection);
+        getCars(lines, numberOfCars ,numberIntersection);
+    }
+
+    public static void getIntersections(final String lines[], final int intersections) {
+        for(int i = 1; i < 1 + intersections; i++) {
+            int startIntersection = Integer.parseInt(lines[i].split(" ")[0]);
+            int endIntersection = Integer.parseInt(lines[i].split(" ")[1]);
+            String streetName = lines[i].split(" ")[2];
+            int L = Integer.parseInt(lines[i].split(" ")[3]);
+            System.out.println(startIntersection);
+            System.out.println(endIntersection);
+            System.out.println(streetName);
+            System.out.println(L +"\n");
+        }
+    }
+
+    public static void getCars(final String lines[], final int cars, final int currentLine) {
+        for(int i = currentLine; i < currentLine + cars; i++) {
+            String[] path = lines[i].split(" ");
+            int numberOfStreets = Integer.parseInt(path[0]);
+            for(int j = 1; j < numberOfStreets; j++) {
+                String StreetToPass = path[j];
+            }
+        }
     }
 }
