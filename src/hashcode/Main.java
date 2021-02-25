e073b518cf9cc32894003318c43368c06700accd
Erik <erik@bigdon.de>
2021-02-25 18:33:07 +0100

    Filemanager
    You can now write into a text file and read from it.

--- src/hashcode/Main.java
+++ src/hashcode/Main.java
@@ -2,8 +2,18 @@ package hashcode;
 
 public class Main {
 
-    public static void main(String[] args) {
+    /**
+     * The main driver of the project.
+     * @param args standard arguments
+     */
+    public static void main(final String[] args) {
         System.out.println("Hello");
-
+        FileManager.createFile("testFile.txt");
+        FileManager.clearFile("testFile.txt");
+        FileManager.writeIntoFile("testFile.txt", "Test line 1");
+        FileManager.readFile("testFile.txt");
+        FileManager.writeIntoFile("testFile.txt", "Test line 2");
+        FileManager.writeIntoFile("testFile.txt", "Test line 3");
+        FileManager.readFile("testFile.txt");
     }
 }
